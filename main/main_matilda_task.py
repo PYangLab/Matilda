f424ec2a296d364d86cb6c6aae79c36de47b497d
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2024-06-19 22:53:56 +1000

    Update main_matilda_task.py

--- main/main_matilda_task.py
+++ main/main_matilda_task.py
@@ -26,14 +26,15 @@ parser.add_argument('--fs', type=bool, default= False, help='if doing feature se
 parser.add_argument('--fs_method', type=str, default= "IntegratedGradient", help='choose the feature selection method')
 parser.add_argument('--dim_reduce', type=bool, default= False, help='save latent space')
 parser.add_argument('--simulation', type=bool, default= False, help='save simulation result')
-parser.add_argument('--simulation_ct', type=int, default= 1, help='save simulation result')
+parser.add_argument('--simulation_ct', type=str, default= "CD16 Mono", help='save simulation result') # if simulation_ct is -1, genarate all cells
 parser.add_argument('--simulation_num', type=int, default= 100, help='save simulation result')
 
 ############# for data build ##############
 parser.add_argument('--rna', metavar='DIR', default='NULL', help='path to train rna data')
 parser.add_argument('--adt', metavar='DIR', default='NULL', help='path to train adt data')
 parser.add_argument('--atac', metavar='DIR', default='NULL', help='path to train atac data')
-parser.add_argument('--cty', metavar='DIR', default='NULL', help='path to train cell type label')
+parser.add_argument('--query_cty', metavar='DIR', default='NULL', help='path to train cell type label')
+parser.add_argument('--ref_cty', metavar='DIR', default='NULL', help='path to train cell type label')
 
 ##############  for training #################
 parser.add_argument('--batch_size', type=int, default=64, help='batch size')
@@ -62,12 +63,16 @@ if args.adt != "NULL" and args.atac != "NULL":
     rna_data_path = args.rna
     adt_data_path = args.adt
     atac_data_path = args.atac
-    label_path = args.cty
+    label_path = args.query_cty
     rna_data = read_h5_data(rna_data_path)
     adt_data = read_h5_data(adt_data_path)
     atac_data = read_h5_data(atac_data_path)
-    label = read_fs_label(label_path)
-    classify_dim = (max(label)+1).cpu().numpy()
+    if label_path == "NULL":
+        label = torch.zeros(rna_data.shape[0]).to(device)
+    else:
+        label = read_fs_label(label_path)
+    ref_label = read_fs_label(args.ref_cty)
+    classify_dim = (max(ref_label)+1).cpu().numpy()
     nfeatures_rna = rna_data.shape[1]
     nfeatures_adt = adt_data.shape[1]
     nfeatures_atac = atac_data.shape[1]
@@ -86,11 +91,15 @@ if args.adt == "NULL" and args.atac != "NULL":
     mode = "SHAREseq"
     rna_data_path = args.rna
     atac_data_path = args.atac
-    label_path = args.cty
+    label_path = args.query_cty
     rna_data = read_h5_data(rna_data_path)
     atac_data = read_h5_data(atac_data_path)
-    label = read_fs_label(label_path)
-    classify_dim = (max(label)+1).cpu().numpy()
+    if label_path == "NULL":
+        label = torch.zeros(rna_data.shape[0]).to(device)
+    else:
+        label = read_fs_label(label_path)
+    ref_label = read_fs_label(args.ref_cty)
+    classify_dim = (max(ref_label)+1).cpu().numpy()
     nfeatures_rna = rna_data.shape[1]
     nfeatures_atac = atac_data.shape[1]
     feature_num = nfeatures_rna  + nfeatures_atac
@@ -106,11 +115,15 @@ if args.adt != "NULL" and args.atac == "NULL":
     mode = "CITEseq"
     rna_data_path = args.rna
     adt_data_path = args.adt
-    label_path = args.cty
+    label_path = args.query_cty
     rna_data = read_h5_data(rna_data_path)
     adt_data = read_h5_data(adt_data_path)
-    label = read_fs_label(label_path)
-    classify_dim = (max(label)+1).cpu().numpy()
+    if label_path == "NULL":
+        label = torch.zeros(rna_data.shape[0]).to(device)
+    else:
+        label = read_fs_label(label_path)
+    ref_label = read_fs_label(args.ref_cty)
+    classify_dim = (max(ref_label)+1).cpu().numpy()
     nfeatures_rna = rna_data.shape[1]
     nfeatures_adt = adt_data.shape[1]
     feature_num = nfeatures_rna + nfeatures_adt
@@ -136,7 +149,7 @@ if args.adt != "NULL":
 if args.atac!= "NULL":
     atac_name  = h5py.File(atac_data_path,"r")['matrix/features'][:]
 
-transform_real_label = real_label(label_path, classify_dim)
+transform_real_label = real_label(args.ref_cty, classify_dim)
 #######build model#########
 if mode == "CITEseq":
     model = CiteAutoencoder_CITEseq(nfeatures_rna, nfeatures_adt, args.hidden_rna, args.hidden_adt, args.z_dim, classify_dim)
@@ -151,9 +164,6 @@ model.eval()
 ########train model#########
 
 if args.classification == True:  
-
-    
-
     if not os.path.exists('../output/classification/{}/{}'.format(mode,path)):
         os.makedirs('../output/classification/{}/{}'.format(mode,path))
     save_path = open('../output/classification/{}/{}/accuracy_each_ct.txt'.format(mode,path),"w")
@@ -162,15 +172,17 @@ if args.classification == True:
     if os.path.exists(checkpoint_tar):
         checkpoint = torch.load(checkpoint_tar)
         model.load_state_dict(checkpoint['state_dict'], strict=True)
-    model, acc1, num1,classified_label, groundtruth_label,prob = test_model(model, dl, transform_real_label, classify_dim = classify_dim, save_path = save_path)
-    average1 = torch.mean(torch.Tensor(acc1))
-    for j in range(len(groundtruth_label)):
-        print('cell ID: ',j, '\t', '\t', 'real cell type:', groundtruth_label[j], '\t', '\t', 'predicted cell type:', classified_label[j], '\t', '\t', 'probability:', round(float(prob[j]),2), file = save_path1)
     
+    model, acc1, num1,classified_label, groundtruth_label,prob = test_model(model, dl, transform_real_label, classify_dim = classify_dim, save_path = save_path)
+    for j in range(data.shape[0]):
+        if args.query_cty!="NULL":
+            print('cell ID: ',j, '\t', '\t', 'real cell type:', groundtruth_label[j], '\t', '\t', 'predicted cell type:', classified_label[j], '\t', '\t', 'probability:', round(float(prob[j]),2), file = save_path1)
+        else:
+            print('cell ID: ',j, '\t', '\t',  'predicted cell type:', classified_label[j], '\t', '\t', 'probability:', round(float(prob[j]),2), file = save_path1)
 
 
-if args.simulation == True:
-    print("simulate celltype index:",  args.simulation_ct, "\t",  "cell type name:", transform_real_label[args.simulation_ct])
+if (args.simulation == True) and (args.simulation_ct!=-1):
+    print("simulate celltype:", args.simulation_ct) #transform_real_label[args.simulation_ct]
     checkpoint_tar = os.path.join(model_save_path, 'model_best.pth.tar')
     if os.path.exists(checkpoint_tar):
         checkpoint = torch.load(checkpoint_tar)
@@ -178,11 +190,12 @@ if args.simulation == True:
     if not os.path.exists('../output/simulation_result/{}/{}/'.format(mode,path)):
         os.makedirs('../output/simulation_result/{}/{}/'.format(mode,path))        
         
-    index = (label == args.simulation_ct).nonzero(as_tuple=True)[0]
+    sim_cty_index = transform_real_label.index(args.simulation_ct)
+    index = (label == sim_cty_index).nonzero(as_tuple=True)[0]
     aug_fold = int(args.simulation_num/int(index.size(0)))    
     remaining_cell = int(args.simulation_num - aug_fold*int(index.size(0)))
 
-    index = (label == args.simulation_ct).nonzero(as_tuple=True)[0]
+    index = (label == sim_cty_index).nonzero(as_tuple=True)[0]
     anchor_data = data[index.tolist(),:]
     anchor_label = label[index.tolist()]
     anchor_transform_dataset = MyDataset(anchor_data, anchor_label)
@@ -223,7 +236,7 @@ if args.simulation == True:
         new_data = torch.cat((new_data,reconstructed_data),0)
         new_label = torch.cat((new_label,reconstructed_label.to(device)),0)
 
-    index = (label != args.simulation_ct).nonzero(as_tuple=True)[0]
+    index = (label != sim_cty_index).nonzero(as_tuple=True)[0]
     anchor_data = data[index.tolist(),:]
     anchor_label = label[index.tolist()]
     real_data = data
@@ -296,7 +309,94 @@ if args.simulation == True:
     pd.DataFrame(real_label_new,  index = cell_name_real, columns = [ "label"]).to_csv( '../output/simulation_result/{}/{}/real_label.csv'.format(mode,path))
 
     print("finish simulation")
+
+if (args.simulation == True) and (args.simulation_ct==-1):
+    print("simulate all cells")
+    checkpoint_tar = os.path.join(model_save_path, 'model_best.pth.tar')
+    if os.path.exists(checkpoint_tar):
+        checkpoint = torch.load(checkpoint_tar)
+        model.load_state_dict(checkpoint['state_dict'], strict=True)
+    if not os.path.exists('../output/simulation_result/{}/{}/'.format(mode,path)):
+        os.makedirs('../output/simulation_result/{}/{}/'.format(mode,path))        
+
+
+
+################################################
+################################################
+################################################
+    sim_data, temp1, temp2, temp3 = model(data)
     
+    sim_label=label
+    real_label=label
+    
+    sim_data_rna = sim_data[:, 0:nfeatures_rna]
+    real_data_rna = data[:, 0:nfeatures_rna]   
+    if mode == "CITEseq":
+        sim_data_adt = sim_data[:, nfeatures_rna:(nfeatures_rna+nfeatures_adt)]
+        real_data_adt = data[:, nfeatures_rna:(nfeatures_rna+nfeatures_adt)]
+    elif mode == "SHAREseq":
+        sim_data_atac = sim_data[:, nfeatures_rna:(nfeatures_rna+nfeatures_atac)]
+        real_data_atac = data[:, nfeatures_rna:(nfeatures_rna+nfeatures_atac)]
+    elif mode == "TEAseq":
+        sim_data_adt = sim_data[:, nfeatures_rna:(nfeatures_rna+nfeatures_adt)]
+        sim_data_atac = sim_data[:, (nfeatures_rna+nfeatures_adt):(nfeatures_rna+nfeatures_adt+nfeatures_atac)]
+        real_data_adt = data[:, nfeatures_rna:(nfeatures_rna+nfeatures_adt)]
+        real_data_atac = data[:, (nfeatures_rna+nfeatures_adt):(nfeatures_rna+nfeatures_adt+nfeatures_atac)]
+        
+    rna_name_new = []
+    adt_name_new = []
+    atac_name_new = []
+    
+    b_list = range(0, real_data_rna.size(0))
+    cell_name_real = ['cell_{}'.format(b) for b in b_list]
+    b_list = range(0, sim_data_rna.size(0))
+    cell_name_sim = ['cell_{}'.format(b) for b in b_list]
+    sim_label_new = []
+    real_label_new = []
+    for j in range(sim_data_rna.size(0)):
+        sim_label_new.append(transform_real_label[sim_label[j]])
+    for j in range(real_data_rna.size(0)):    
+        real_label_new.append(transform_real_label[real_label[j]])
+    for i in range(sim_data_rna.size(1)):
+        a = str(rna_name[i],encoding="utf-8")
+        rna_name_new.append(a)           
+        
+    if mode == "CITEseq":
+        for i in range(sim_data_adt.size(1)):
+            a = str(adt_name[i],encoding="utf-8")
+            adt_name_new.append(a)
+        pd.DataFrame(sim_data_adt.cpu().detach().numpy(), index = cell_name_sim, columns = adt_name_new).to_csv( '../output/simulation_result/{}/{}/sim_data_adt.csv'.format(mode,path))
+        pd.DataFrame(real_data_adt.cpu().detach().numpy(), index = cell_name_real, columns = adt_name_new).to_csv( '../output/simulation_result/{}/{}/real_data_adt.csv'.format(mode,path))
+
+        
+    if mode == "SHAREseq":
+        for i in range(sim_data_atac.size(1)):
+            a = str(atac_name[i],encoding="utf-8")
+            atac_name_new.append(a)           
+        pd.DataFrame(sim_data_atac.cpu().detach().numpy(), index = cell_name_sim, columns = atac_name_new).to_csv( '../output/simulation_result/{}/{}/sim_data_atac.csv'.format(mode,path))
+        pd.DataFrame(real_data_atac.cpu().detach().numpy(), index = cell_name_real, columns = atac_name_new).to_csv( '../output/simulation_result/{}/{}/real_data_atac.csv'.format(mode,path))
+
+        
+    if mode == "TEAseq":
+        for i in range(sim_data_adt.size(1)):
+            a = str(adt_name[i],encoding="utf-8")
+            adt_name_new.append(a)
+        for i in range(sim_data_atac.size(1)):
+            a = str(atac_name[i],encoding="utf-8")
+            atac_name_new.append(a)            
+        pd.DataFrame(sim_data_adt.cpu().detach().numpy(), index = cell_name_sim, columns = adt_name_new).to_csv( '../output/simulation_result/{}/{}/sim_data_adt.csv'.format(mode,path))
+        pd.DataFrame(real_data_adt.cpu().detach().numpy(), index = cell_name_real, columns = adt_name_new).to_csv( '../output/simulation_result/{}/{}/real_data_adt.csv'.format(mode,path))
+        pd.DataFrame(sim_data_atac.cpu().detach().numpy(), index = cell_name_sim, columns = atac_name_new).to_csv( '../output/simulation_result/{}/{}/sim_data_atac.csv'.format(mode,path))
+        pd.DataFrame(real_data_atac.cpu().detach().numpy(), index = cell_name_real, columns = atac_name_new).to_csv( '../output/simulation_result/{}/{}/real_data_atac.csv'.format(mode,path))
+
+    pd.DataFrame(sim_data_rna.cpu().detach().numpy(), index = cell_name_sim, columns = rna_name_new).to_csv( '../output/simulation_result/{}/{}/sim_data_rna.csv'.format(mode,path))
+    pd.DataFrame(real_data_rna.cpu().detach().numpy(), index = cell_name_real, columns = rna_name_new).to_csv( '../output/simulation_result/{}/{}/real_data_rna.csv'.format(mode,path))
+    pd.DataFrame(sim_label_new,  index = cell_name_sim, columns = [ "label"]).to_csv( '../output/simulation_result/{}/{}/sim_label.csv'.format(mode,path))
+    pd.DataFrame(real_label_new,  index = cell_name_real, columns = [ "label"]).to_csv( '../output/simulation_result/{}/{}/real_label.csv'.format(mode,path))
+
+    print("finish simulation")
+    
+        
 if args.dim_reduce == True:
     checkpoint_tar = os.path.join(model_save_path, 'model_best.pth.tar')
     if os.path.exists(checkpoint_tar):
@@ -372,7 +472,6 @@ if args.fs == True:
 
         if not os.path.exists(save_fs_eachcell):
             os.makedirs(save_fs_eachcell)   
-        pd.DataFrame(attribution_mean.cpu().numpy(), index = features, columns = [ "importance score"]).to_csv(save_fs_eachcell+"/fs."+"celltype"+str(i)+".csv")
+        pd.DataFrame(attribution_mean.cpu().numpy(), index = features, columns = [ "importance score"]).to_csv(save_fs_eachcell+"/fs."+"celltype_"+transform_real_label[i]+".csv")
     print("finish feature selection")
     
-
